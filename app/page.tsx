ba29fc73833aeff09261d992d817f35efc1183e5
Daniel <betapudidaniel.in@gmail.com>
2026-08-12 12:41:00 +0530

    setup-01

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <button>
+        Get Started
+      </button>
+    </div>
+  );
+}
